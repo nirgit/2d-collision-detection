86fceefd78420b92e5dfac2ae5f632be49910816
Nir Moav <getnirm@gmail.com>
2016-04-28 15:15:53 +0300

    2nd commit - dividing and sorting.

--- src/collision.js
+++ src/collision.js
@@ -1,3 +1,54 @@
-function add(a, b) {
-  return a + b;
+'use strict';
+
+function noop() {}
+
+function getGridBuckets(squareDim, factor) {
+  var squares = [];
+  for (var i = 0; i < factor; i++) {
+    for(var j = 0; j < factor; j++) {
+      squares.push({
+        x: j * squareDim.w,
+        y: i * squareDim.h,
+        groupA: [],
+        groupB: []
+      });
+    }
+  }
+  return squares;
+}
+
+function setToMatchingGridBucket(bucketDimension, factor, gridBuckets, type, element) {
+  var j = Math.floor(element.x / bucketDimension.w);
+  var i = Math.floor(element.y / bucketDimension.h);
+
+  var bucket = gridBuckets[j + i * factor];
+  bucket[type].push(element);
+}
+
+/**
+  *
+  */
+function handleCollision(groupA, groupB, gridWidth, gridHeight, factor, collisionHandleCB) {
+  if (!gridWidth || gridWidth <= 0 || !gridHeight || gridHeight <= 0) {
+    throw new Error('Grid dimensions must be valid and non-negative');
+  }
+  groupA = groupA || [];
+  groupB = groupB || [];
+  factor = Math.max(Math.round(factor) || 1, 2);
+  collisionHandleCB = collisionHandleCB || noop;
+
+  var squareDim = {w: gridWidth / factor, h: gridHeight / factor};
+  var gridBuckets = getGridBuckets(squareDim, factor);
+
+  var groupAMatcher = setToMatchingGridBucket.bind(null, squareDim, factor, gridBuckets, 'groupA');
+  var groupBMatcher = setToMatchingGridBucket.bind(null, squareDim, factor, gridBuckets, 'groupB');
+
+  groupA.forEach(groupAMatcher);
+  groupB.forEach(groupBMatcher);
+
+  gridBuckets.forEach(function(bucket) {
+    console.log('x = ' + bucket.x + ', y = ' + bucket.y);
+    console.log('groupA = ' + bucket.groupA.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
+    console.log('groupB = ' + bucket.groupB.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
+  });
 }
--- test/collision.spec.js
+++ test/collision.spec.js
@@ -1,5 +1,13 @@
 describe('foo', function() {
   it('should', function() {
-    expect(add(1, 2)).toBe(2);
+    var groupA = [{x: 5, y: 58}, {x: 97, y: 15}, {x: 20, y: 201}, {x: 152, y: 152}];
+    var groupB = [{x: 65, y: 58}, {x: 27, y: 25}, {x: 120, y: 241}, {x: 52, y: 192}, {x: 170, y: 40}];
+    var gridWidth = 200;
+    var gridHeight = 300;
+    var factor = 2;
+    var cb = function() {};
+
+
+    expect(handleCollision(groupA, groupB, gridWidth, gridHeight, factor, cb)).toBe(2);
   });
 });
