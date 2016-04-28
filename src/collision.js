964f585cd9359962e32198c78fe9486d06181a73
Nir Moav <getnirm@gmail.com>
2016-04-28 16:06:03 +0300

    collision detection with optimization!

--- src/collision.js
+++ src/collision.js
@@ -18,11 +18,35 @@ function getGridBuckets(squareDim, factor) {
 }
 
 function setToMatchingGridBucket(bucketDimension, factor, gridBuckets, type, element) {
-  var j = Math.floor(element.x / bucketDimension.w);
-  var i = Math.floor(element.y / bucketDimension.h);
+  var j = Math.max(0, Math.floor(element.x / bucketDimension.w));
+  var i = Math.max(0, Math.floor(element.y / bucketDimension.h));
 
   var bucket = gridBuckets[j + i * factor];
   bucket[type].push(element);
+}
+
+function handleCollisionInBucket(collisionHandleCB, gridBucket) {
+  var groupA = gridBucket.groupA;
+  var groupB = gridBucket.groupB;
+  groupA.sort(byX);
+  groupB.sort(byX);
+
+  for (var i = 0; i < groupA.length; i++) {
+    var objA = groupA[i];
+    for (var j = 0; j < groupB.length; j++) {
+      var objB = groupB[j];
+      if ((objB.x >= objA.x && objB.x <= objA.x + objA.w &&
+          objB.y >= objA.y && objB.y <= objA.y + objA.h) ||
+          (objA.x >= objB.x && objA.x <= objB.x + objB.w &&
+              objA.y >= objB.y && objA.y <= objB.y + objB.h)) {
+            collisionHandleCB(objA, objB);
+          }
+    }
+  }
+}
+
+function byX(obj1, obj2) {
+  return Math.sign(obj1.x - obj2.x);
 }
 
 /**
@@ -34,7 +58,7 @@ function handleCollision(groupA, groupB, gridWidth, gridHeight, factor, collisio
   }
   groupA = groupA || [];
   groupB = groupB || [];
-  factor = Math.max(Math.round(factor) || 1, 2);
+  factor = Math.max(Math.round(factor) || 1, 1);
   collisionHandleCB = collisionHandleCB || noop;
 
   var squareDim = {w: gridWidth / factor, h: gridHeight / factor};
@@ -46,9 +70,11 @@ function handleCollision(groupA, groupB, gridWidth, gridHeight, factor, collisio
   groupA.forEach(groupAMatcher);
   groupB.forEach(groupBMatcher);
 
-  gridBuckets.forEach(function(bucket) {
-    console.log('x = ' + bucket.x + ', y = ' + bucket.y);
-    console.log('groupA = ' + bucket.groupA.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
-    console.log('groupB = ' + bucket.groupB.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
-  });
+  gridBuckets.forEach(handleCollisionInBucket.bind(null, collisionHandleCB));
+
+  // gridBuckets.forEach(function(bucket) {
+  //   console.log('x = ' + bucket.x + ', y = ' + bucket.y);
+  //   console.log('groupA = ' + bucket.groupA.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
+  //   console.log('groupB = ' + bucket.groupB.reduce(function(a, v) {return a + ' (' + v.x + ',' + v.y + ')';}, ''));
+  // });
 }
--- test/collision.spec.js
+++ test/collision.spec.js
@@ -1,13 +1,52 @@
+function createRandomGroup(numberOfObjs, maxW, maxH) {
+  var group = [];
+  for (var i = 0 ; i < numberOfObjs; i++) {
+    var randX = Math.floor(Math.random() * maxW);
+    var randY = Math.floor(Math.random() * maxH);
+    group.push({x: randX, y: randY});
+  }
+  return group;
+}
+
+
 describe('foo', function() {
   it('should', function() {
-    var groupA = [{x: 5, y: 58}, {x: 97, y: 15}, {x: 20, y: 201}, {x: 152, y: 152}];
-    var groupB = [{x: 65, y: 58}, {x: 27, y: 25}, {x: 120, y: 241}, {x: 52, y: 192}, {x: 170, y: 40}];
-    var gridWidth = 200;
-    var gridHeight = 300;
-    var factor = 2;
-    var cb = function() {};
+    var gridWidth = 10000;
+    var gridHeight = 10000;
+
+    var groupA = createRandomGroup(5000, gridWidth, gridHeight);
+    var groupB = createRandomGroup(4000, gridWidth, gridHeight);
+
+    var groupAObjDims = {w: 4, h: 10};
+    var groupBObjDims = {w: 25, h: 25};
+
+    groupA = groupA.map(function(objA) {
+      objA.x -= (groupAObjDims.w / 2);
+      objA.y -= (groupAObjDims.h / 2);
+      objA.w = groupAObjDims.w;
+      objA.h = groupAObjDims.h;
+      return objA;
+    });
+
+    groupB = groupB.map(function(objB) {
+      objB.x -= (groupBObjDims.w / 2);
+      objB.y -= (groupBObjDims.h / 2);
+      objB.w = groupBObjDims.w;
+      objB.h = groupBObjDims.h;
+      return objB;
+    });
 
 
-    expect(handleCollision(groupA, groupB, gridWidth, gridHeight, factor, cb)).toBe(2);
+    var factor = 5;
+    var collisions = [];
+    var cb = function(objA, objB) {
+      collisions.push({a: objA, b: objB});
+      // console.log('collision: ' + JSON.stringify(objA) + ' | ' + JSON.stringify(objB));
+    };
+
+    var start = Date.now();
+    handleCollision(groupA, groupB, gridWidth, gridHeight, factor, cb);
+    var end = Date.now();
+    console.log('total time:' + (end-start) + 'ms');
   });
 });
